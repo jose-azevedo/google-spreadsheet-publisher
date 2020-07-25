4b3fe7b06d3c699b596aa1de8033ca2b218196a8
José Roberto Azevedo <66346043+joseazevedo47@users.noreply.github.com>
2020-07-25 19:57:15 -0300

    Updates files via recieved HTTP request

--- index.js
+++ index.js
@@ -16,7 +16,6 @@ app.get('/google/auth', (req, res) => {
 });
 
 app.post('/upload', (req, res) => {
-  
   listFiles(oAuth2Client, req.body, uploadFiles);
   res.status(200).end()
 });
@@ -26,16 +25,16 @@ var server = app.listen(5000, () => {
 });
 
 // If modifying these scopes, delete token.json.
-const SCOPES = ['https://www.googleapis.com/auth/drive.metadata.readonly','https://www.googleapis.com/auth/drive.file','https://www.googleapis.com/auth/drive.metadata','https://www.googleapis.com/auth/drive.readonly'];
+const SCOPES = ['https://www.googleapis.com/auth/drive.file','https://www.googleapis.com/auth/drive.metadata'];
 
 const TOKEN_PATH = 'token.json';
 
 fs.readFile('credentials.json', (err, content) => {
   if (err) return console.log('Error loading client secret file:', err);
-  authorize();
+  authorize(listFiles);
 });
 
-function authorize() {
+function authorize(callback) {
   fs.readFile(TOKEN_PATH, (err, token) => {
     if (err) return getAccessToken(oAuth2Client, callback);
     oAuth2Client.setCredentials(JSON.parse(token));
@@ -75,53 +74,48 @@ function listFiles(auth, body, callback) {
     fields: 'nextPageToken, files(id, name)',
   }, (err, res) => {
     if (err) return console.log('The API returned an error: ' + err);
-    const files = res.data.files.map((file) => {return file.name});
+    var files = res.data.files.map((file) => {return file.name});
+    var ids = res.data.files.map((file) => {return file.id});
+    var fileExists;
+    var id;
     console.log(files);
+    if(files.indexOf(body.name) > -1) {
+      console.log('Ta no array');
+      fileExists = true;
+      id = ids[files.indexOf(body.name)];
+    } else {
+      fileExists = false;
+      console.log('não ta no array');
+    }
 
-    callback(auth, body.name, body.data);
-    // if (files.length) {
-    //   console.log('Files:');
-    //   files.map((file) => {
-    //     console.log(`${file.name} (${file.id})`);
-    //   });
-    // } else {
-    //   console.log('No files found.');
-    // }
+    callback(auth, id, body.name, body.data, fileExists);
   });
 }
 
-function uploadFiles(auth, filename, filedata) {
-  const drive = google.drive({version: 'v3', auth});
-  var fileMetadata = {
-    'name': filename
+function uploadFiles(auth, id, filename, filedata, fileExists) {
+  const sheets = google.sheets({version: 'v4', auth});
+  console.log(`Filename: ${filename}`);
+  console.log(`id: ${id}`);
+  console.log(`data: ${filedata}`);
+  var data = filedata.replace(/\.+/g, ',');
+  data = data.split(';');
+  console.log(`data: ${data}`);
+  let values = [
+    data
+  ];
+  const resource = {
+    values,
   };
-  var media = {
-    mimeType: 'text/csv',
-    body: filedata
-  };
-  drive.files.create({
-    resource: fileMetadata,
-    media: media,
-    fields: 'id'
-  }, function (err, file) {
+  sheets.spreadsheets.values.append ({
+    spreadsheetId: id,
+    range: 'A1:H1',
+    resource,
+    valueInputOption: 'USER_ENTERED'
+  }, (err, result) => {
     if (err) {
-      console.error(err);
+      console.log(err);
     } else {
-      console.log('File Id: ', file.data.id);
+      console.log('%d cells updated.', result.updatedCells);
     }
   });
 }
-
-//function uploadFilesAxios(auth, filename, filedata)
-
-  // axios({
-  //   method: 'post',
-  //   url: 'https://www.googleapis.com/upload/drive/v3/files?uploadType=media',
-  //   headers: {
-  //     'X-Requested-With': 'XMLHttpRequest'
-  //   },
-  //   data: {
-  //     firstName: 'Fred',
-  //     lastName: 'Flintstone'
-  //   }
-  // });
